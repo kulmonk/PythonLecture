5018f4569949e0bc039d22d02354fc7d255d9816
kulrao <kulrao@gmail.com>
2024-05-27 04:36:30 +0530

    first commit

--- hello.py
+++ hello.py
@@ -17,9 +17,40 @@ print(msg1)
 
 print(len(message))
 
+#slicing
 print(message[0])
 
 print(message[10])
 
 print(message[0:5])
 
+print(message[0:5]) # first index is inclusive and second index is not 
+
+
+print(message[:5])
+
+print(message[6:])
+
+
+print(message.lower())
+
+print(message.upper())
+
+print(message.count('H'))
+
+print(message.find('w'))
+
+new_msg = message.replace('world', 'kuldeep')
+
+print(new_msg)
+
+greenting ="Good Morning"
+name = "kuldeep"
+
+cont_msg = greenting + ','+ name
+
+print(cont_msg)
+
+cont_msg = '{}, {}. Welcome !'.format(greenting,name)
+
+print(cont_msg)
